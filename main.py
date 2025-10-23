dd7ae799684c7c7744698c6228b97d2ea91ce7eb
stepanaromanov <stepanaromanov@gmail.com>
2025-10-23 17:29:30 +0500

    daily-sync | 2025-10-23 17:29 |        1 files updated

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.utils.utils_general import setup_logging
 import logging
 import configs.logging_config
 from departments import ceo
-# from departments import education
+from departments import education
 from departments import finance
 from departments import hr
 from departments import marketing
