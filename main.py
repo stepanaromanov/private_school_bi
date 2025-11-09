cfafd4ebff0848a199a13eb6761194fe2a45f503
stepanaromanov <stepanaromanov@gmail.com>
2025-11-09 22:08:55 +0500

    daily-sync | 2025-11-09 22:08 |        2 files updated

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import logging
 from configs import logging_config
+
+
 from departments import ceo
 from departments import education
 from departments import finance
 from departments import hr
 from departments import marketing
 from departments import sales
+
+
+def main():
+    logging.info("ETL Application started")
+
+
+if __name__ == "__main__":
+    main()
